98c995754c46e3150ed3ac00808e08db1e76feed
3R11K <124263623+3R11K@users.noreply.github.com>
2023-08-07 11:02:36 -0300

    respostas

--- exercicio1.py
+++ exercicio1.py
@@ -5,12 +5,11 @@
 
 
 # Crie a função que será avaliada no exercício aqui
-
-
-
-
-
-
+def conta_palavras_unicas(text):
+    text = text.split()
+    if text.duplicated():
+        return False
+    else True
 
 # Teste a sua função aqui (caso ache necessário)
 
--- exercicio2.py
+++ exercicio2.py
@@ -5,13 +5,13 @@
 
 
 # Crie a função que será avaliada no exercício aqui
-
-
-
-
-
-
-
+def is_anagram(string1, string2):
+    if len(string1) != len(string2):
+        return False
+    for i in string1:
+        if i not in string2:
+            return False
+    return True
 # Teste a sua função aqui (caso ache necessário)
 
 
